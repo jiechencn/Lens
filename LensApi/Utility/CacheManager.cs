983d79300aa54e0ece2ea418e4c0b3d601ae71ca
Jie Chen <jiechencn@qq.com>
2023-09-23 12:58:19 +0800

    add cache manager

---
+++ LensApi/Utility/CacheManager.cs
@@ -0,0 +1,81 @@
+﻿using System.Threading.Tasks;
+using System;
+using Microsoft.Extensions.Caching.Memory;
+using System.Threading;
+
+namespace Me.JieChen.Lens.Api.Utility;
+
+public sealed class CacheManager
+{
+    private static int CacheSizeLimit = 1024;
+    private static int CacheEntrySizeLimit = 1;
+    private static int ExpirationScanIntervalInMinute = 10;
+    private static int AbsoluteExpirationInMinute = 7 * 24 * 60;
+    private static int SlidingExpirationInMinute = 100 * 60;
+
+    private static CacheManager? instance;
+
+    private readonly static SemaphoreSlim instanceLock = new SemaphoreSlim(1);
+    private readonly SemaphoreSlim cacheLock = new SemaphoreSlim(1);
+    private readonly IMemoryCache cache;
+    private readonly MemoryCacheEntryOptions entryOptions;
+
+    private CacheManager()
+    {
+        cache = new MemoryCache
+        (
+            new MemoryCacheOptions
+            {
+                ExpirationScanFrequency = TimeSpan.FromMinutes(ExpirationScanIntervalInMinute),
+                SizeLimit = CacheSizeLimit
+            }
+        );
+
+        entryOptions = new MemoryCacheEntryOptions
+        {
+            AbsoluteExpirationRelativeToNow = TimeSpan.FromMinutes(AbsoluteExpirationInMinute),
+            SlidingExpiration = TimeSpan.FromMinutes(SlidingExpirationInMinute),
+            Size = CacheEntrySizeLimit
+        };
+    }
+    public static CacheManager GetInstance()
+    {
+        if (instance != null)
+        {
+            return instance;
+        }
+
+        try
+        {
+            instanceLock.WaitAsync().Wait();
+            instance = new CacheManager();
+            return instance;
+        }
+        finally
+        {
+            instanceLock.Release();
+        }
+    }
+
+    public async Task<TValue> GetAsync<TKey, TValue>(TKey key, Func<Task<TValue>> callbackIfNotFroundFromCache)
+        where TKey : class
+        where TValue : class
+    {
+        if (cache.TryGetValue(key, out TValue value))
+        {
+            return value;
+        }
+
+        try
+        {
+            await cacheLock.WaitAsync().ConfigureAwait(false);
+            TValue newValue = await callbackIfNotFroundFromCache.Invoke().ConfigureAwait(false);
+            cache.Set<TValue>(key, newValue, entryOptions);
+            return newValue;
+        }
+        finally
+        {
+            cacheLock.Release();
+        }
+    }
+}
---
+++ LensApiTest/Utility/CacheManagerTest.cs
@@ -0,0 +1,73 @@
+﻿using Me.JieChen.Lens.Api.Utility;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Moq;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Test.JieChen.Lens.Api.Utility;
+
+[TestClass]
+public class CacheManagerTest
+{
+    [TestMethod]
+    public void GetInstance_TwoInstances_Should_Be_Same()
+    {
+        // Act
+        CacheManager cacheManager1 = CacheManager.GetInstance();
+        CacheManager cacheManager2 = CacheManager.GetInstance();
+
+        // Assert
+        Assert.AreEqual(cacheManager1.GetHashCode(), cacheManager2.GetHashCode());
+    }
+
+    [TestMethod]
+    public void GetAsync_Twice_Should_Be_Same()
+    {
+        // Arrange
+        string key = "mykey";
+        Mock<AbstractFoo> mockFoo = new Mock<AbstractFoo>();
+
+        // Act
+        var result1 = CacheManager.GetInstance().GetAsync<string, Student>(key, () => ReadFromExternalDataSourceAsync(key, mockFoo.Object)).Result;
+        // Assert
+        mockFoo.Verify(f => f.Do(), Times.Once);
+
+        // Arrange
+        mockFoo.Invocations.Clear();
+        // Act
+        var result2 = CacheManager.GetInstance().GetAsync<string, Student>(key, () => ReadFromExternalDataSourceAsync(key, mockFoo.Object)).Result;
+        // Assert
+        mockFoo.Verify(f => f.Do(), Times.Never);
+
+        // Assert
+        Assert.AreEqual(result1, result2);
+    }
+
+    private async Task<Student> ReadFromExternalDataSourceAsync(string key, AbstractFoo foo)
+    {
+        foo.Do();
+        return await Task.FromResult(new Student { Name = "foo", Age = 20 }).ConfigureAwait(false);
+    }
+
+    private class Student
+    {
+        public string Name { get; set; }
+        public int Age { get; set; }
+    }
+}
+
+public abstract class AbstractFoo
+{
+    public AbstractFoo()
+    {
+
+    }
+    public virtual void Do()
+    {
+
+    }
+}
+
